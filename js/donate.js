da47cee25f7761910b0279918e13441d02e65021
babladey275 <babladey275@gmail.com>
2024-09-23 22:53:52 +0600

    added event handler feni & quota section

--- js/donate.js
+++ js/donate.js
@@ -4,16 +4,42 @@ const donateButtons = document.getElementsByClassName('btn-donate-now');
 for(let i = 0; i < donateButtons.length; i++) {
     donateButtons[i].addEventListener('click', function(){
         const amountNoakhali = getInputValueById('input-amount-noakhali');
+        const amountFeni = getInputValueById('input-amount-feni');
+        const amountInjured = getInputValueById('input-amount-injured');
 
         if(amountNoakhali > 0) {
+            console.log('a');
             const balance = getTextFieldValueById('account-balance');
-            const noakhaliFloodBalance = getTextFieldValueById('noakhali-flood-balance');
+            const noakhaliAccountBalance = getTextFieldValueById('noakhali-account-balance');
 
             const newBalance = balance - amountNoakhali;
-            const newNoakhaliFloodBalance = noakhaliFloodBalance + amountNoakhali;
+            const newNoakhaliAccountBalance = noakhaliAccountBalance + amountNoakhali;
 
             document.getElementById('account-balance').innerText = newBalance;
-            document.getElementById('noakhali-flood-balance').innerText = newNoakhaliFloodBalance;
+            document.getElementById('noakhali-account-balance').innerText = newNoakhaliAccountBalance;
+            return;
+        }
+        else if(amountFeni > 0){
+            console.log('b');
+            const balance = getTextFieldValueById('account-balance');
+            const feniAccountBalance = getTextFieldValueById('feni-account-balance');
+
+            const newBalance = balance - amountFeni;
+            const newFeniAccountBalance = feniAccountBalance + amountFeni;
+
+            document.getElementById('account-balance').innerText = newBalance;
+            document.getElementById('feni-account-balance').innerText = newFeniAccountBalance;
+        }
+        else if(amountInjured > 0){
+            console.log('c');
+            const balance = getTextFieldValueById('account-balance');
+            const injuredAccountBalance = getTextFieldValueById('injured-account-balance');
+
+            const newBalance = balance - amountInjured;
+            const newInjuredAccountBalance = injuredAccountBalance + amountInjured;
+
+            document.getElementById('account-balance').innerText = newBalance;
+            document.getElementById('injured-account-balance').innerText = newInjuredAccountBalance;
         }
         else{
             alert('Invalid amount');
